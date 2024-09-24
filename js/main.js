5f39b94f6db5e6a81cb5f2013345cbd6f4faaf2f
korosuke3116 <2005doi0531@gmail.com>
2024-09-24 23:48:04 +0900

    季節表示までできた

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-
-let index = 0;
-
-
-let flower_element = document.querySelectorAll('ul#flower-list li');
-
-
-for (let i = 0; i < flower_element.length; i++) {
-    flower_element[i].addEventListener('click', function() {
-        index = flower_element[i].data
-        showModal(index);
-    });
-}
-
-
-const modal = document.getElementById('modal');
-
-function showModal(idx) {
-    modal.style.display = 'block';
-}
-
-const close = document.querySelector('.close-bg');
-
-close.addEventListener('click', function() {
-    modal.style.display = 'none';
-=======
 let index = 0;
 
 
@@ -78,4 +52,47 @@ document.getElementById('loadtab-2').addEventListener('click', () => {
         console.error('エラー:', error);
       });
 });
+
+document.querySelector('.mySelect').addEventListener('change', function() {
+  const selectedValue = this.value;
+  const styledElement = document.querySelector('.season');
+  
+  if (selectedValue === '1') {
+      styledElement.classList.add('styled');
+  } else {
+      styledElement.classList.remove('styled');
+  }
+});
+
+document.querySelector('.mySelect').addEventListener('change', function() {
+  const selectedValue = this.value;
+  const styledElement = document.querySelector('.month');
+  
+  if (selectedValue === '1') {
+      styledElement.classList.add('styled');
+  } else {
+      styledElement.classList.remove('styled');
+  }
+});
+
+document.querySelector('.mySelect').addEventListener('change', function() {
+  const selectedValue = this.value;
+  const styledElement = document.querySelector('.kind');
+  
+  if (selectedValue === '1') {
+      styledElement.classList.add('styled');
+  } else {
+      styledElement.classList.remove('styled');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const initialValue = document.querySelector('.mySelect').value;
+  const styledElement = document.querySelector('.season');
+  
+  if (initialValue === '1') {
+      styledElement.classList.add('styled');
+  }
+});
+
 
